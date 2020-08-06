d613b1a8d0504fe6b574edf031c8559d7e2ce7ef
Leonardo Vivanco <leonardo.vivanco@wetek.com>
2020-08-06 22:41:53 +0100

    Add Routes

--- src/API_Routes.js
+++ src/API_Routes.js
@@ -1,4 +1,5 @@
-const PROTOCOL = "http";
-const API_DOMAIN = `${PROTOCOL}://localhost:3004`;
+const PROTOCOL = "https";
+const API_DOMAIN = `${PROTOCOL}://test-basf.herokuapp.com`;
 export const API_MENU_LIST = `${API_DOMAIN}/menu`;
-export const API_ITEMS_LIST = `${API_DOMAIN}/items`;
+export const API_ITEMS_csv1 = `${API_DOMAIN}/csv1`;
+export const API_ITEMS_csv2 = `${API_DOMAIN}/csv2`;
