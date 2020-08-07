89b0b25239e951fecb6760a7bdcfb338ab785b0c
Leonardo Vivanco <leonardo.vivanco@wetek.com>
2020-08-07 21:57:52 +0100

    Change theme

--- src/App.js
+++ src/App.js
@@ -3,15 +3,27 @@ import { BrowserRouter as Router } from "react-router-dom";
 import Drawler from "./components/Drawler";
 import ContentsPages from "./components/ContentsPages";
 import GlobalProvider from "./context/GlobalProvider";
-
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#074950",
+    },
+    secondary: {
+      main: "#F2F79E",
+    },
+  },
+});
 const App = () => {
   return (
     <GlobalProvider>
-      <Router>
-        <Drawler title="BASF CHALLANGE!">
-          <ContentsPages />
-        </Drawler>
-      </Router>
+      <ThemeProvider theme={theme}>
+        <Router>
+          <Drawler title="BASF CHALLENGE!">
+            <ContentsPages />
+          </Drawler>
+        </Router>
+      </ThemeProvider>
     </GlobalProvider>
   );
 };
